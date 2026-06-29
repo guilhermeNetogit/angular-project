89a7dfd383388e9abbc615d9e69398c26766b9f1
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-06-29 16:40:02 -0300

    feat(usuarios): refactor course list type, add material styles and dynamic routing. - Change 'cursos' data structure from string array to object type (ID and Name). - Implement custom SCSS input styles and Angular Material list. - Configure dynamic child routing and input binding to display course details.

--- src/app/usuarios/usuarios.ts
+++ src/app/usuarios/usuarios.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
-import { UsuariosDetalhe } from "./usuarios-detalhe/usuarios-detalhe";
-import { CursosService } from './services/cursos';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Curso, CursosService } from './services/cursos';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [UsuariosDetalhe],
+  imports: [MatListModule, RouterLink, RouterOutlet],
   templateUrl: './usuarios.html',
   styleUrl: './usuarios.scss',
 })
 export class Usuarios {
-  cursos: string[] = [];
+  cursos: Curso[] = [];
 
-  constructor (public cursosService: CursosService) {
+  constructor(public cursosService: CursosService) {
     this.cursos = this.cursosService.getCursos();
   }
 
-  onAddCurso(curso: string) {
-    this.cursosService.addCurso(curso);
+  onAddCurso(nomeCurso: string) {
+    if (!nomeCurso.trim()) return;
+
+    const novoCurso: Curso = {
+      id: this.cursos.length + 1,
+      nomeCurso: nomeCurso,
+    };
+
+    this.cursosService.addCurso(novoCurso);
+    this.cursos = this.cursosService.getCursos();
+  }
+
+  action(nomeCurso: string) {
+    console.log('Curso clicado:', nomeCurso);
   }
 }
