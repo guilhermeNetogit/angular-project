601a5a228447b446e4e97001716acd908a81e8f6
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-07-10 14:43:06 -0300

    feat(form): Auto-populate address using CEP code (setValue and patchValue). - Auto-populating fields with data retrieved from an external CEP code lookup API service. - Validation of the entered CEP code with an alert.

--- src/app/components/form-component/form.ts
+++ src/app/components/form-component/form.ts
@@ -23,31 +23,79 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
-      nome: ['', [Validators.required, Validators.minLength(3),
-      Validators.maxLength(50)
-      ]],
+      nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
-      //endereco: {
-        cep: ['',[Validators.maxLength(8)]],
+      ddd: [''],
+      telefone: [],
+      endereco: this.formBuilder.group({
+        cep: ['', [Validators.pattern(/^[0-9]{5}-?[0-9]{3}$/)]],
         numero: [''],
         complemento: [''],
         logradouro: [''],
         bairro: [''],
         cidade: [''],
-        uf: ['']
-     // }
+        uf: [''],
+      }),
+    });
+  }
+
+  aplicarMascaraCep(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let valor = input.value.replace(/\D/g, ''); // Remove tudo que não for número
+
+    if (valor.length > 5) {
+      valor = `${valor.slice(0, 5)}-${valor.slice(5, 8)}`;
+    }
+
+    // Sincroniza o valor formatado com o FormGroup do Angular
+    this.formulario.get('endereco.cep')?.setValue(valor, { emitEvent: false });
+  }
+
+  buscarCep() {
+    const cep = this.formulario.get('endereco.cep')?.value;
+
+    const cepLimpo = cep?.replace(/\D/g, '');
+
+    if (cepLimpo && cepLimpo.length === 8) {
+      this.http.get<any>(`https://viacep.com.br/ws/${cepLimpo}/json/`).subscribe({
+        next: (dados) => {
+          if (!dados.erro) {
+            this.formulario.get('ddd')?.setValue(dados.ddd);
+            this.formulario.get('endereco')?.patchValue({
+              logradouro: dados.logradouro,
+              complemento: dados.complemento,
+              bairro: dados.bairro,
+              cidade: dados.localidade,
+              uf: dados.uf,
+            });
+          } else {
+            alert('CEP não encontrado.');
+            this.resetarDadosForm();
+          }
+        },
+        error: (erro) => {
+          console.error('Erro ao buscar o CEP:', erro);
+          this.resetarDadosForm();
+        },
+      });
+    }
+  }
+  private resetarDadosForm(): void {
+    this.formulario.get('endereco')?.patchValue({
+      logradouro: '',
+      complemento: '',
+      bairro: '',
+      cidade: '',
+      uf: '',
     });
   }
 
   enviarDados(): void {
     if (this.formulario.valid) {
       console.log('Dados do formulário:', this.formulario.value);
-
-      // Aqui você envia os dados para a sua API / Service como JSON automaticamente
       this.http.post(this.urlApi, this.formulario.value).subscribe({
         next: (dados) => {
           console.log('Resposta da API', dados);
-
           this.formulario.reset();
         },
         error: (erro) => {
