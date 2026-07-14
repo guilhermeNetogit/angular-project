5fd5093e793643fca66af3949546c7b8c716c24e
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-07-14 13:32:19 -0300

    feat(server): add REST Server. - Testing with JSON-Server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
+registerLocaleData(localePt);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes, withComponentInputBinding())
-  ]
+    provideRouter(appRoutes, withComponentInputBinding()),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
 };
